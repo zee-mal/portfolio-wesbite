f75fbbabfb1cf6ac714c09c0948157453e36a0ec
Ziphezinhle <omuhlemalinga17@gmail.com>
2024-10-17 11:06:59 +0200

    Added a js file

---
+++ script.js
@@ -0,0 +1,17 @@
+let name="Hello there"
+name="haha"
+
+let areas= Math.pow(5,2);
+
+const person ={
+    name:"Lerato",
+    surname:"Msholozi",
+    
+printName:()=>{
+    console.log("I'm a function")
+    }
+
+}
+
+const retValue=person.printName()
+console.log(retValue)
